3aa91c85e5b06c4db7f73825c563c97194c5e726
Theo <theo.poette@epitech.eu>
2021-08-23 12:27:57 +0200

    feat can upload post with caption and any file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import firebase from 'firebase/app';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -10,15 +10,48 @@ import { AuthService } from '../services/auth.service';
 })
 export class HomeComponent {
   caption: string = '';
-  constructor(private auth: AuthService) { }
+  fileName: string = '';
+  file: File | null = null ;
+  constructor(public auth: AuthService, private storage: AngularFireStorage) { }
+
+  async fileUploader(): Promise<String> {
+    const path = `post/${this.auth.user?.uid}/${Date.now()}`;
+    const ref = this.storage.ref(path);
+    const task = await this.storage.upload(path, this.file);
+    return this.storage.ref(path).getDownloadURL().toPromise();
+  }
 
   submitPost(): void {
-    firebase.firestore()
-      .collection('posts')
-      .doc(this.auth.user?.uid)
-      .collection('userPosts')
-      .add({
-        caption: this.caption,
-      });
+    if (!this.caption && !this.file)
+      return;
+
+    //file handling
+    if (!this.file) {
+      firebase.firestore()
+        .collection('posts')
+        .doc(this.auth.user?.uid)
+        .collection('userPosts')
+        .add({
+          caption: this.caption,
+        });
+        return;
+    }
+    this.fileUploader().then(url => {
+      firebase.firestore()
+        .collection('posts')
+        .doc(this.auth.user?.uid)
+        .collection('userPosts')
+        .add({
+          caption: this.caption,
+          file: url,
+        });
+    });
+  }
+
+  onFileSelected(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    this.file = file;
+    this.fileName = file.name;
   }
 }
