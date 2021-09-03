929d2aafe7a3d1c9e9d8b5f3210c240dfed12d71
Theo <theo.poette@epitech.eu>
2021-09-03 13:28:50 +0200

    deploy: fix non used tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.{html,ts}",
+      "./projects/**/*.{html,ts}"
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
